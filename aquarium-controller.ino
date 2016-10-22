60fc269828e6377d00e6aa0e29f9cbd321d54be8
jbannink <j.bannink@gmail.com>
2016-10-22 22:44:24 +0200

    Update aquarium-controller.ino
    timekeeping tests

--- aquarium-controller.ino
+++ aquarium-controller.ino
@@ -1,7 +1,6 @@
-
 #include <Wire.h>
 //use pin scl and scda on arduino uno
-#include <LiquidCrystal_I2C.h>
+//#include <LiquidCrystal_I2C.h>
 //for storing data past boot
 #include <EEPROM.h>
 //for using tiny rtc
@@ -11,53 +10,41 @@ RTC_DS1307 RTC;
 #define BACKLIGHT_PIN     13
 //Gebruik i2cguess om niet werkende lcd's uit te lezen
 //Gebruik de library van F Malpartida voor aansturing van de LCD
-LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+//LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
-int photocellPin = 1;// Digital Pin the photocell is connected to is A1
-int photocellReading;
-int lichtsterkte;
+//int lichtsterkte;
 byte waarde;
-byte controller1_aanuur;
-byte controller1_aanminuut;
-byte controller1_uituur;
-byte controller1_uitminuut;
+
 int address = 0;
-int c1_aanuur = 1;
-int c1_uituur = 2;
+int c1_aanuur = 22;
+int c1_aanminuut = 43;
+int c1_uituur = 23;
+int c1_uitminuut = 15;
 int c2_aanuur = 3;
 int c2_uituur = 4;
 byte button1_state = LOW;
 int button1_pin = 2;
-
+const int c1_redPin = 22;
+const int c1_greenPin = 23;
+const int c1_bluePin = 24;
+int uurnu;
+int minuutnu;
+int secnu;
 
 /////////////////////////////
 //SETUP
 void setup()
 {
-  //LCD setup
-pinMode ( BACKLIGHT_PIN, OUTPUT );
-  digitalWrite ( BACKLIGHT_PIN, LOW );
-  pinMode(button1_pin, INPUT);
-  lcd.begin(16,2);               // initialize the lcd 
-  lcd.home ();                   // go home
-  lcd.print("Hello, ARDUINO ");  
-  lcd.setCursor ( 0, 1 );        // go to the next line
-  lcd.setCursor(0,0);
-  delay(500);
-  lcd.print (" Booting....  ");
-  delay(500);
-  lcd.setCursor(0,0);
-  lcd.print ("             ");
-  delay(500);
-  lcd.setCursor(0,0);
-  lcd.print("lichtsterkte -");
+  pinMode(c1_redPin,OUTPUT);
+  pinMode(c1_greenPin,OUTPUT);
+  pinMode(c1_bluePin,OUTPUT);
   //waarde van de eeprom uitlezen
   waarde = EEPROM.read(address); 
   //rtc setup
   Serial.begin(57600);
   Wire.begin();
   RTC.begin();
-  //als de klok niet klopt, zet rtc.adjust dan buiten het if statement, upload, en zet rt.adjust terug in het if statement en upload.
+    //als de klok niet klopt, zet rtc.adjust dan buiten het if statement, upload, en zet rt.adjust terug in het if statement en upload.
   //Op deze manier wordt de tijd gezet met de tijd dat het programma gecompileerd werd
   if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
@@ -72,7 +59,7 @@ void loop()
   {
     //rtc function
     DateTime now = RTC.now();
-   /* Serial.print(now.year(), DEC);
+    Serial.print(now.year(), DEC);
     Serial.print('/');
     Serial.print(now.month(), DEC);
     Serial.print('/');
@@ -89,7 +76,7 @@ void loop()
     Serial.print("s = ");
     Serial.print(now.unixtime() / 86400L);
     Serial.println("d");//End rtc function
-  */
+
   //read buttons en wijzig waarde 0-23
   button1_state = digitalRead(button1_pin);
   if (button1_state == HIGH)
@@ -104,12 +91,35 @@ void loop()
   EEPROM.write(address, waarde);   
   //  lichtsterkte uitlezen
   digitalWrite ( BACKLIGHT_PIN, LOW );
-  photocellReading = analogRead(photocellPin); 
-  lichtsterkte=1023-photocellReading;
-  lcd.setCursor(0,1);
-  lcd.print(lichtsterkte);lcd.print(" ") ;lcd.print(waarde);lcd.print(" ");lcd.print(button1_state);
-  delay(500);
-  lcd.setCursor(0,0);
-  lcd.print(now.day());lcd.print("/");lcd.print(now.month());lcd.print("/");lcd.print(now.year());lcd.print(" ");lcd.print(now.hour());lcd.print(":");lcd.print(now.minute());
+  uurnu=now.hour();
+  minuutnu=now.minute();
+  secnu=now.second();
+  Serial.print("uurnu = ");
+  Serial.print(uurnu);
+if ((uurnu == c1_aanuur) && (minuutnu == c1_aanminuut) && (secnu <=2))
+  {
+  sunup();
   }
+if ((uurnu == c1_uituur) && (minuutnu == c1_uitminuut) && (secnu <= 2)) 
+  {
+  sundown();
+  }
+}
+
+void sunup() {
+  for(int fadeValue=0; fadeValue<=254; fadeValue++) {
+  analogWrite(c1_greenPin, fadeValue);
+  analogWrite(c1_redPin, fadeValue);
+  analogWrite(c1_bluePin, fadeValue);
+  delay(50);
+  }
+}
+void sundown() {
+  for(int fadeValue=254; fadeValue>=0; fadeValue--) {
+  analogWrite(c1_greenPin, fadeValue);
+  analogWrite(c1_redPin, fadeValue);
+  analogWrite(c1_bluePin, fadeValue);
+  delay(50);
+  }
+}
 
