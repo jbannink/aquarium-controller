cf77a19c6d0f5d498cbe68f1d2985961d20b8faf
jbannink <j.bannink@gmail.com>
2016-11-10 23:45:36 +0100

    Add files via upload

--- aquarium-controller.ino
+++ aquarium-controller.ino
@@ -1,3 +1,9 @@
+// IMPORTANT: Adafruit_TFTLCD LIBRARY MUST BE SPECIFICALLY
+// CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
+// SEE RELEVANT COMMENTS IN Adafruit_TFTLCD.h FOR SETUP.
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
 #include <Wire.h>
 //use pin scl and scda on arduino uno
 //#include <LiquidCrystal_I2C.h>
@@ -11,10 +17,45 @@ RTC_DS1307 RTC;
 //Gebruik i2cguess om niet werkende lcd's uit te lezen
 //Gebruik de library van F Malpartida voor aansturing van de LCD
 //LiquidCrystal_I2C lcd(0x3f, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+//tft-settings
+
+#define LCD_CS A3 // Chip Select goes to uintAnalog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+// For the Arduino Mega, use digital pins 22 through 29
+// (on the 2-row header at the end of the board).
+
+// Assign human-readable names to some common 16-bit color values:
+#define  BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+// If using the shield, all control and data lines are fixed, and
+// a simpler declaration can optionally be used:
+// Adafruit_TFTLCD tft;
 
 //int lichtsterkte;
 byte waarde;
-
+//kloksettings
 int address = 0;
 int c1_aanuur = 19;
 int c1_aanminuut =15;
@@ -35,6 +76,8 @@ int secnu;
 //SETUP
 void setup()
 {
+  tft.reset();
+  uint16_t identifier = 0x9341;
 // pinMode(c1_redPin,OUTPUT);
 // pinMode(c1_greenPin,OUTPUT);
 // pinMode(c1_bluePin,OUTPUT);
@@ -52,6 +95,14 @@ void setup()
     // following line sets the RTC to the date & time this sketch was compiled
     RTC.adjust(DateTime(__DATE__, __TIME__));
   }
+  tft.begin(identifier);
+
+  Serial.println(F("Benchmark                Time (microseconds)"));
+
+  Serial.print(F("Screen fill              "));
+  Serial.println(testFillScreen());
+  delay(500);
+
  }
 
 ////////////////////////////
@@ -77,7 +128,12 @@ void loop()
     Serial.print("s = ");
     Serial.print(now.unixtime() / 86400L);
     Serial.println("d");//End rtc function
-
+  // testFillScreen();
+    tft.setCursor(0,0);
+    tft.setTextColor(WHITE);
+     tft.println("Datum");
+    tft.print(now.day(), DEC);tft.print("/");tft.print(now.month(),DEC);tft.print("/");tft.println(now.day(),DEC);
+    tft.print(now.hour(), DEC);tft.print(":");tft.println(now.minute(), DEC);
   //read buttons en wijzig waarde 0-23
   button1_state = digitalRead(button1_pin);
   if (button1_state == HIGH)
@@ -128,4 +184,12 @@ void sundown() {
     }
 }
 
-
+unsigned long testFillScreen() {
+ // unsigned long start = micros();
+  tft.fillScreen(BLACK);
+  tft.fillScreen(RED);
+  tft.fillScreen(GREEN);
+  tft.fillScreen(BLUE);
+  tft.fillScreen(BLACK);
+ // return micros() - start;
+}
