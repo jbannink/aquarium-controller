97f6f6a9c658ab947b48dee462e60a1d062b2f07
jbannink <j.bannink@gmail.com>
2016-11-15 22:41:35 +0100

    Add files via upload

--- aquarium-controller.ino
+++ aquarium-controller.ino
@@ -1,9 +1,10 @@
-// IMPORTANT: Adafruit_TFTLCD LIBRARY MUST BE SPECIFICALLY
+  // IMPORTANT: Adafruit_TFTLCD LIBRARY MUST BE SPECIFICALLY
 // CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
 // SEE RELEVANT COMMENTS IN Adafruit_TFTLCD.h FOR SETUP.
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD.h> // Hardware-specific library
+#include <TouchScreen.h>
 #include <Wire.h>
 //use pin scl and scda on arduino uno
 //#include <LiquidCrystal_I2C.h>
@@ -24,6 +25,25 @@ RTC_DS1307 RTC;
 #define LCD_WR A1 // LCD Write goes to Analog 1
 #define LCD_RD A0 // LCD Read goes to Analog 0
 #define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+//touch Settings
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 23   // can be a digital pin
+#define XP 22   // can be a digital pin
+//changed YM 9 and XP 8 to represent Mega
+#define TS_MINX 200
+#define TS_MINY 200
+#define TS_MAXX 910
+#define TS_MAXY 915
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+#define PENRADIUS 3
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 
 // When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
 // For the Arduino Uno, Duemilanove, Diecimila, etc.:
@@ -55,14 +75,24 @@ Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 
 //int lichtsterkte;
 byte waarde;
+int teller=0;
 //kloksettings
-int address = 0;
-int c1_aanuur = 19;
-int c1_aanminuut =15;
-int c1_uituur = 19;
-int c1_uitminuut = 10;
-int c2_aanuur = 3;
-int c2_uituur = 4;
+int adrc1aanuur = 0;
+int adrc1aanmin =1;
+int adrc1uituur = 2;
+int adrc1uitmin = 3;
+int adrc2aanuur = 4;
+int adrc2aanmin = 5;
+int adrc2uituur = 6;
+int adrc2uitmin = 8;
+int c1_aanuur = EEPROM.read(adrc1aanuur);
+int c1_aanmin =EEPROM.read(adrc1aanmin);
+int c1_uituur = EEPROM.read(adrc1uituur);
+int c1_uitmin = EEPROM.read(adrc1uitmin);
+int c2_aanuur = EEPROM.read(adrc2aanuur);
+int c2_aanmin =EEPROM.read(adrc2aanmin);
+int c2_uituur = EEPROM.read(adrc2uituur);
+int c2_uitmin = EEPROM.read(adrc2uitmin);
 byte button1_state = LOW;
 int button1_pin = 2;
 const int c1_redPin = 44;
@@ -76,13 +106,14 @@ int secnu;
 //SETUP
 void setup()
 {
+   pinMode(13, OUTPUT);//???voor touch??
   tft.reset();
   uint16_t identifier = 0x9341;
 // pinMode(c1_redPin,OUTPUT);
 // pinMode(c1_greenPin,OUTPUT);
 // pinMode(c1_bluePin,OUTPUT);
   //waarde van de eeprom uitlezen
-  waarde = EEPROM.read(address); 
+ // waarde = EEPROM.read(address); 
   //rtc setup
   Serial.begin(57600);
   Wire.begin();
@@ -100,7 +131,8 @@ void setup()
   Serial.println(F("Benchmark                Time (microseconds)"));
 
   Serial.print(F("Screen fill              "));
-  Serial.println(testFillScreen());
+  //Serial.println(testFillScreen());
+   tft.fillScreen(BLACK);
   delay(500);
 
  }
@@ -109,55 +141,109 @@ void setup()
 //LOOP
 void loop()
   {
-    //rtc function
-    DateTime now = RTC.now();
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(' ');
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    Serial.print(" since 1970 = ");
-    Serial.print(now.unixtime());
-    Serial.print("s = ");
-    Serial.print(now.unixtime() / 86400L);
-    Serial.println("d");//End rtc function
-  // testFillScreen();
-    tft.setCursor(0,0);
-    tft.setTextColor(WHITE);
-     tft.println("Datum");
-    tft.print(now.day(), DEC);tft.print("/");tft.print(now.month(),DEC);tft.print("/");tft.println(now.day(),DEC);
-    tft.print(now.hour(), DEC);tft.print(":");tft.println(now.minute(), DEC);
-  //read buttons en wijzig waarde 0-23
-  button1_state = digitalRead(button1_pin);
-  if (button1_state == HIGH)
-     {
-     waarde=waarde+1;
-     if (waarde == 24) 
-        {
-        waarde=0;
-        }
-     }
+  //
+  tft.setCursor(0,100);tft.print("Change Clock 1");
+  tft.fillCircle(100, 100, 16, GREEN);
+  tft.setCursor(0,150);tft.print("Change Clock 2");
+  tft.fillCircle(100,150,16,BLUE);
+  tft.setCursor(0,200);tft.print("set Uit");
+  tft.fillCircle(100,200,16,RED);
+  digitalWrite(13, HIGH);
+  TSPoint p = ts.getPoint();
+  digitalWrite(13, LOW);
+  // if sharing pins, you'll need to fix the directions of the touchscreen pins
+  //pinMode(XP, OUTPUT);
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+  //pinMode(YM, OUTPUT);
+
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+     
+     // scale from 0->1023 to tft.width
+     p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+     p.y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0);
+     Serial.print("X = "); Serial.print(p.x);
+     Serial.print("\tY = "); Serial.print(p.y);
+     Serial.print("\tPressure = "); Serial.println(p.z);
+     if (p.y > 88  and p.y < 116 and p.x > 125 and p.x < 166) {
+         Serial.println("Change Clock 1");
+         Serial.print("("); Serial.print(p.x);
+         Serial.print(", "); Serial.print(p.y);
+         Serial.println(")");
+         ChangeTime(c1_aanuur,c1_aanmin,c1_uituur,c1_uitmin);
+      } 
+  
+      if (p.y > 138  and p.y < 166 and p.x > 125 and p.x < 166) {
+         Serial.println("Change Clock 2");
+         Serial.print("("); Serial.print(p.x);
+         Serial.print(", "); Serial.print(p.y);
+         Serial.println(")");
+         ChangeTime(c2_aanuur,c2_aanmin,c2_uituur,c2_uitmin);
+  
+      }
+      
+      if (p.y > 188  and p.y < 216 and p.x > 125 and p.x < 166) {
+         Serial.println("red");
+         Serial.print("("); Serial.print(p.x);
+         Serial.print(", "); Serial.print(p.y);
+         Serial.println(")");
+      }
+   }
+   //rtc function
+   DateTime now = RTC.now();
+   /*   Serial.print(now.year(), DEC);
+   Serial.print('/');
+   Serial.print(now.month(), DEC);
+   Serial.print('/');
+   Serial.print(now.day(), DEC);
+   Serial.print(' ');
+   Serial.print(now.hour(), DEC);
+   Serial.print(':');
+   Serial.print(now.minute(), DEC);
+   Serial.print(':');
+   Serial.print(now.second(), DEC);
+   Serial.println();
+   Serial.print(" since 1970 = ");
+   Serial.print(now.unixtime());
+   Serial.print("s = ");
+   Serial.print(now.unixtime() / 86400L);
+   Serial.println("d");*/
+   //End rtc function
+   /* tft.setCursor(0,0);
+   tft.setTextColor(WHITE);
+   tft.setTextSize(2); 
+   tft.println("Datum : ");
+   tft.setTextColor(RED);
+   tft.setTextSize(2);
+   tft.print(now.day(), DEC);tft.print("/");tft.print(now.month(),DEC);tft.print("/");tft.println(now.year(),DEC);
+   tft.setTextColor(GREEN);
+   tft.print(now.hour(), DEC);tft.print(":");tft.println(now.minute(), DEC); /*
+    
+   /*read buttons en wijzig waarde 0-23
+   button1_state = digitalRead(button1_pin);
+   if (button1_state == HIGH)
+      {
+      waarde=waarde+1;
+      if (waarde == 24) 
+         {
+         waarde=0;
+         }
+       }*/
+     
   //gewijzigde waarde opslaan in eeprom   
-  EEPROM.write(address, waarde);   
+ // EEPROM.write(address, waarde);   
   //  lichtsterkte uitlezen
   digitalWrite ( BACKLIGHT_PIN, LOW );
   uurnu=now.hour();
   minuutnu=now.minute();
   secnu=now.second();
-  Serial.print("uurnu = ");
-  Serial.print(uurnu);
-if ((uurnu == c1_aanuur) && (minuutnu == c1_aanminuut) && (secnu <=2))
+  //Serial.print("uurnu = ");
+  //Serial.print(uurnu);
+if ((uurnu == c1_aanuur) && (minuutnu == c1_aanmin) && (secnu <=2))
   {
   sunup();
   }
-if ((uurnu == c1_uituur) && (minuutnu == c1_uitminuut) && (secnu <= 2)) 
+if ((uurnu == c1_uituur) && (minuutnu == c1_uitmin) && (secnu <= 2)) 
    {
     sundown();
    }
@@ -192,4 +278,66 @@ unsigned long testFillScreen() {
   tft.fillScreen(BLUE);
   tft.fillScreen(BLACK);
  // return micros() - start;
+}
+
+unsigned long testFilledCircles(uint8_t radius, uint16_t color) {
+  unsigned long start;
+  int x, y, w = tft.width(), h = tft.height(), r2 = radius * 2;
+
+  tft.fillScreen(BLACK);
+  start = micros();
+  for (x = radius; x < w; x += r2) {
+    for (y = radius; y < h; y += r2) {
+
+    }
+  }
+
+  return micros() - start;
+}
+void ChangeTime(int aanuur,int aanmin,int uituur,int uitmin) {
+  tft.fillScreen(BLUE);
+  tft.setCursor(0,10);tft.print("Change Clock 1");
+  tft.fillCircle(10, 10, 8, YELLOW);
+  tft.setCursor(0,20);tft.print(aanuur);
+  tft.fillCircle(10,30,8,YELLOW);
+  while (teller <1){
+    TSPoint p = ts.getPoint();
+    if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
+     
+       // scale from 0->1023 to tft.width
+       p.x = map(p.x, TS_MINX, TS_MAXX, tft.width(), 0);
+       p.y = map(p.y, TS_MINY, TS_MAXY, tft.height(), 0);
+       Serial.print("X = "); Serial.print(p.x);
+       Serial.print("\tY = "); Serial.print(p.y);
+       Serial.print("\tPressure = "); Serial.println(p.z);
+       if (p.y > 10  and p.y < 16 and p.x > 10 and p.x < 20) {
+           Serial.print("("); Serial.print(p.x);
+           Serial.print(", "); Serial.print(p.y);
+           Serial.println(")");
+           aanuur=aanuur+1;
+           if (aanuur ==23);
+             {
+              aanuur=0;
+           }
+         }
+     
+         if (p.y > 138  and p.y < 166 and p.x > 125 and p.x < 166) {
+           Serial.println("Change Clock 2");
+           Serial.print("("); Serial.print(p.x);
+           Serial.print(", "); Serial.print(p.y);
+           Serial.println(")");
+           ChangeTime(c2_aanuur,c2_aanmin,c2_uituur,c2_uitmin);
+         }
+      
+         if (p.y > 188  and p.y < 216 and p.x > 125 and p.x < 166) {
+           Serial.println("red");
+           Serial.print("("); Serial.print(p.x);
+           Serial.print(", "); Serial.print(p.y);
+           Serial.println(")");
+           teller=teller+1;
+           Serial.println(teller);
+         }
+      }
+  }
+  teller=0;
 }
